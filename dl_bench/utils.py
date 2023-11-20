1021f861615a6fa9a1bd9e41808a8da0f9d09f7b
Petr Kurapov <petr.a.kurapov@intel.com>
2023-11-20 12:36:44 -0700

    Add cpu-proto torch-mlir backend

--- dl_bench/utils.py
+++ dl_bench/utils.py
@@ -5,6 +5,58 @@ import torch
 from torch.nn import Module
 
 
+from typing import Any
+import numpy as np
+
+
+def recursively_convert_to_numpy(o: Any):
+    if isinstance(o, torch.Tensor):
+        return o.numpy()
+    if isinstance(o, tuple):
+        return tuple(recursively_convert_to_numpy(x) for x in o)
+    if isinstance(o, list):
+        return [recursively_convert_to_numpy(x) for x in o]
+    if isinstance(o, dict):
+        return {k: recursively_convert_to_numpy(v) for k, v in o.items()}
+    # No-op cases. Explicitly enumerated to avoid things sneaking through.
+    if isinstance(o, str):
+        return o
+    if isinstance(o, float):
+        return o
+    if isinstance(o, int):
+        return o
+    raise Exception(f"Unexpected Python function input: {o}")
+
+def recursively_convert_from_numpy(o: Any):
+    if isinstance(o, np.ndarray):
+        return torch.from_numpy(o)
+    if isinstance(o, tuple):
+        return tuple(recursively_convert_from_numpy(x) for x in o)
+    if isinstance(o, list):
+        return [recursively_convert_from_numpy(x) for x in o]
+    if isinstance(o, dict):
+        return {k: recursively_convert_from_numpy(v) for k, v in o.items()}
+    # No-op cases. Explicitly enumerated to avoid things sneaking through.
+    if isinstance(o, str):
+        return o
+    if isinstance(o, float):
+        return o
+    if isinstance(o, int):
+        return o
+    raise Exception(f"Unexpected Python function output: {o}")
+
+
+
+def refine_result_type(_result):
+    if isinstance(_result, tuple):
+        return tuple(refine_result_type(x) for x in _result)
+    elif isinstance(_result, np.ndarray):
+        return torch.from_numpy(_result)
+    elif isinstance(_result, (bool, int, float)):
+        return _result
+    else:
+        raise ValueError(f"Unhandled return type {type(_result)}")
+
 class TimerManager:
     def __init__(self):
         self.name2time = {}
@@ -76,11 +128,44 @@ class Backend:
                 model, fullgraph=True, dynamic=False, mode="reduce-overhead"
             )
             print("Compiled with dynamo")
-        elif compile_mode == "torch_mlir":
+        elif compile_mode == "torch_mlir_default_inference":
             import torch._dynamo as dynamo
             from dl_bench.dynamo_be import torch_mlir_be as be
 
             compiled_model = dynamo.optimize(be.refbackend_torchdynamo_backend)(model)
+            print("Compiled with torch_mlir (torchscript, inference)")
+        elif compile_mode == "torch_mlir":
+            from torch_mlir._dynamo_fx_importer import import_fx_graph_as_func
+            from torch_mlir_e2e_test.configs.torchdynamo import jit
+            from torch_mlir_e2e_test.framework import TestOptions
+            # from torch_mlir_e2e_test.linalg_on_tensors_backends.refbackend import RefBackendLinalgOnTensorsBackend
+            from torch_mlir_e2e_test.linalg_on_tensors_backends.cpuprotobackend import CpuProtoLinalgOnTensorsBackend
+            import torch.utils._pytree as pytree
+
+            opts = TestOptions()
+            module = jit(model, [sample_input], 'test_name', opts, output_type="linalg-on-tensors")
+            backend = CpuProtoLinalgOnTensorsBackend(opts)
+            # backend = RefBackendLinalgOnTensorsBackend()
+            module = backend.compile(module)
+            backend_module = backend.load(module)
+
+            params = {
+                    **dict(model.named_parameters(remove_duplicate=False)),
+                    **dict(model.named_buffers(remove_duplicate=False)),
+            }
+            params_flat, params_spec = pytree.tree_flatten(params)
+            params_flat = list(params_flat)
+
+            class result:
+                def __call__(self, *args):
+                    numpy_inputs = recursively_convert_to_numpy(params_flat + [*args])
+                    return refine_result_type(getattr(backend_module,
+                            model.__class__.__name__)(*numpy_inputs))
+
+                def eval(self):
+                    pass
+
+            compiled_model = result()
             print("Compiled with torch_mlir")
         else:
             raise ValueError(f"Unsupported mode {compile_mode}")
