a4a76a25d3b9a88801fd9d843b8b65fa3f7fa3d5
Gregory Shimansky <gshimansky@gmail.com>
2023-12-11 20:06:29 +0000

    Removed debug actions
    Signed-off-by: Gregory Shimansky <gshimansky@gmail.com>

--- tests/scripts/build-mlir.sh
+++ tests/scripts/build-mlir.sh
@@ -8,17 +8,6 @@ fi
 
 
 cd $1
-git submodule init
-git submodule update --recursive --init
-git submodule foreach --recursive 'git fetch --tags'
-ls -la
-pwd
-git status
-pushd externals/llvm-project
-ls -la
-pwd
-git status
-popd
 
 ${CONDA}/bin/conda env create -n mlir -f conda-dev-env.yml
 ${CONDA}/bin/conda install -n mlir -y pip
