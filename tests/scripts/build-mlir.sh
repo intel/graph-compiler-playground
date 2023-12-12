376ac7ef7295618da12fe514de6b5d3741751709
Egor Krivov <egor.krivov@intel.com>
2023-12-12 16:25:44 +0100

    Torchvision install

--- tests/scripts/build-mlir.sh
+++ tests/scripts/build-mlir.sh
@@ -14,7 +14,7 @@ ${CONDA}/bin/conda install -n mlir -y pip
 source ${CONDA}/bin/activate mlir
 
 pip install -r requirements.txt
-# This might require an updat
-pip install --pre torch==2.2.0.dev20231105 torchvision==0.17.0.dev20231105+cpu --index-url https://download.pytorch.org/whl/nightly/cpu
+# This might require an update
+pip install --pre torch==2.2.0.dev20231105 torchvision==0.17.0.dev20231105+cpu --index-url https://download.pytorch.org/whl/nightly/cpu || echo "Failed to install torchvision"
 
 bash ./utils/build-with-imex.sh
