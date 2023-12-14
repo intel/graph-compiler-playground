8309ee08bc365e40798ddf2c8a81cc6e1d85266c
Gregory Shimansky <gshimansky@users.noreply.github.com>
2023-12-14 11:31:46 -0600

    Added support for cloud based runners in AWS (#35)
    Co-authored-by: Egor Krivov <egor.krivov@intel.com>

---
+++ db_tools/export_csv2url.py
@@ -0,0 +1,46 @@
+import argparse
+import numpy as np
+import pandas as pd
+
+from dl_bench.report import BenchmarkDb
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'path',
+        default="results.csv",
+        help="Path to results.csv",
+    )
+    # Backend
+    parser.add_argument(
+        "--host",
+        default=None,
+        help="Name of the host machine",
+    )
+    # Reporting
+    parser.add_argument("-t", "--tag", default="", help="Tag to mark this result in DB")
+    parser.add_argument(
+        "-u",
+        "--url",
+        default=None,
+        help="Database url in sqlalchemy format, like sqlite://./database.db'",
+    )
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    df = pd.read_csv(args.path)
+    if args.host is not None:
+        df['host'] = args.host
+
+    db = args.url and BenchmarkDb(args.url)
+
+    for i, row in df.drop(['id', 'date'], axis=1).iterrows():
+        if db is None:
+            print(row, end='\n\n')
+        else:
+            db.report(**row)
+
+if __name__ == '__main__':
+    main()
---
+++ db_tools/export_sqlite2csv.sh
@@ -0,0 +1,1 @@
+sqlite3 -header -csv results.db "select * from torchmlir_benchmark;" > results.csv
