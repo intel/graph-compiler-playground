336a2b1855e2b5c009893b068eb4489d0328a2ea
Gregory Shimansky <gshimansky@gmail.com>
2023-12-15 19:31:05 +0000

    Split test scripts into cnn and mlp
    Signed-off-by: Gregory Shimansky <gshimansky@gmail.com>

---
+++ cnn.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+export ONEDNN_VERBOSE=0
+
+if [[ -z "${DL_BENCH_ARGS}" ]]; then
+  echo "Please, provide DL_BENCH_ARGS environment variable"
+  exit 1
+fi
+
+CNNS=(vgg16 resnet18 resnet50 resnext50 resnext101 densenet121 efficientnet_v2m mobilenet_v3_large)
+for BS in 0001 0032 0128
+do
+    for name in "${CNNS[@]}"
+    do
+        echo "Benchmark $name"
+        benchmark-run -b cnn -p "name='${name}',batch_size='$BS'" --benchmark_desc "${name}_bs$BS" ${DL_BENCH_ARGS} || echo Failed
+    done
+done
