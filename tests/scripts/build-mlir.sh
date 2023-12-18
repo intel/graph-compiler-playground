d8b7944b2ead494561348d96df6aade55ff7617f
Gregory Shimansky <gshimansky@gmail.com>
2023-12-18 17:16:36 +0000

    Updated mlir conda env name
    Signed-off-by: Gregory Shimansky <gshimansky@gmail.com>

--- tests/scripts/build-mlir.sh
+++ tests/scripts/build-mlir.sh
@@ -6,14 +6,14 @@ if (( $# != 1 )); then
     >&2 echo "Need path to torch-mlir repository as an argument."
 fi
 
-if ${CONDA}/bin/conda env list | grep mlir > /dev/null; then
+if ${CONDA}/bin/conda env list | grep mlir-dev > /dev/null; then
     echo "mlir conda environment already exists from cache, not creating a new one."
 else
     SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
     ${SCRIPT_DIR}/create-mlir-env.sh $1
 fi
 
-source ${CONDA}/bin/activate mlir
+source ${CONDA}/bin/activate mlir-dev
 
 cd $1
 bash ./utils/build-with-imex.sh
--- tests/scripts/create-mlir-env.sh
+++ tests/scripts/create-mlir-env.sh
@@ -8,6 +8,6 @@ fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ${SCRIPT_DIR}/create-env.sh $1/conda-dev-env.yml
-source ${CONDA}/bin/activate mlir
+source ${CONDA}/bin/activate mlir-dev
 pip install -r $1/requirements.txt
 pip install -r $1/torchvision-requirements.txt
