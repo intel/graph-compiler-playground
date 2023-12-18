f619a29183a68acb74699a42da58a1b28201c9c8
Gregory Shimansky <gshimansky@users.noreply.github.com>
2023-12-18 11:33:10 -0600

    Fixed mlir build and conda cache (#47)
    Signed-off-by: Gregory Shimansky <gshimansky@gmail.com>

--- tests/scripts/create-mlir-env.sh
+++ tests/scripts/create-mlir-env.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -ex
+
 if (( $# != 1 )); then
     >&2 echo "Need path to torch-mlir repository as an argument."
 fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ${SCRIPT_DIR}/create-env.sh $1/conda-dev-env.yml
-source ${CONDA}/bin/activate mlir
+source ${CONDA}/bin/activate mlir-dev
 pip install -r $1/requirements.txt
 pip install -r $1/torchvision-requirements.txt
