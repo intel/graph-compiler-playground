79d3f148ef97926135ccd3bece84a78b23da4b39
Egor <egor.krivov@intel.com>
2023-12-28 11:38:42 +0100

    [IPEX]: added xpu device and bfloat16 (#37)

--- dl_bench/utils.py
+++ dl_bench/utils.py
@@ -133,10 +133,8 @@ class Backend:
         self.dtype = str_to_dtype(dtype)
 
     def to_device(self, x: torch.Tensor):
-        if self.device_name == "cuda":
+        if self.device_name in ("cuda", 'xpu'):
             return x.to(self.device)
-        elif self.device_name == "xpu":
-            raise NotImplementedError("xpu have no to_device impl yet.")
         elif self.device_name == "cpu":
             return x
         else:
@@ -169,9 +167,10 @@ class Backend:
             compiled_model = torch.jit.freeze(compiled_model)
             print("Compiled with torchscript onednn")
         elif compile_mode == "ipex":
-            import intel_extension_for_pytorch
+            import intel_extension_for_pytorch as ipex
 
-            compiled_model = intel_extension_for_pytorch.optimize(model, sample_input=sample_input)
+            params = {} if dtype != torch.bfloat16 else {'dtype': torch.bfloat16}
+            compiled_model = ipex.optimize(model, sample_input=sample_input, **params)
             print("Compiled with ipex")
         elif compile_mode == "ipex_onednn_graph":
             import intel_extension_for_pytorch as ipex
@@ -215,9 +214,9 @@ class Backend:
             )
             import torch.utils._pytree as pytree
 
-            #            debug_timer seems to cause problems:
-            #            TypeError: TestOptions.__init__() got an unexpected keyword argument 'debug_timer'
-            #            opts = TestOptions(debug_timer=False, use_kernels=True)
+            # debug_timer seems to cause problems:
+            # TypeError: TestOptions.__init__() got an unexpected keyword argument 'debug_timer'
+            # opts = TestOptions(debug_timer=False, use_kernels=True)
             opts = TestOptions(use_kernels=True)
             module = jit(
                 model,
@@ -260,8 +259,7 @@ class Backend:
         device_name = device_name.lower()
         # TODO: do we really need this import?
         if device_name == "xpu":
-            import intel_extension_for_pytorch
-
+            return 'xpu'
         if device_name in ("cpu", "xpu", "cuda"):
             device = torch.device(device_name)
             return device
