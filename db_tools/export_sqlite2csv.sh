7b3655fb7cb5ee62a9e0d64c42cf48122646134a
Egor Krivov <egor.krivov@intel.com>
2023-12-28 12:49:55 +0100

    fixed bugs

--- db_tools/export_sqlite2csv.sh
+++ db_tools/export_sqlite2csv.sh
@@ -1,3 +1,2 @@
 DB_PATH="${1:-results.db}"
-sqlite3 -header -csv results.db "select * from torchmlir_benchmark;" > results.csv
-                  name: ${{ steps.set_up_vars.outputs.conda_env }}.db
+sqlite3 -header -csv ${DB_PATH}.db "select * from torchmlir_benchmark;" > results.csv
