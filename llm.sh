c5b4d3fc6d9bc15593597fdce702902b90c80738
Egor Krivov <egor.krivov@intel.com>
2024-01-08 05:59:09 +0100

    github actions update

--- llm.sh
+++ llm.sh
@@ -9,5 +9,5 @@ if [[ -z "${DL_BENCH_ARGS}" ]]; then
   exit 1
 fi
 
-echo "Bfloat16 on size5"
-benchmark-run -b llm -p "" --benchmark_desc "gptj" --dtype bfloat16 ${DL_BENCH_ARGS} || echo Failed
+benchmark-run -b llm -p "" --benchmark_desc "gptj" --dtype float32 ${DL_BENCH_ARGS} || echo Failed
+benchmark-run -b llm -p "" --benchmark_desc "gptj_bfloat16" --dtype bfloat16 ${DL_BENCH_ARGS} || echo Failed
