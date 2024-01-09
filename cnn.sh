608a5ed7542b404dda373ab1b0c2c4937e9806af
Egor Krivov <egor.krivov@intel.com>
2024-01-09 11:05:29 +0100

    Added bfloat16 run

--- cnn.sh
+++ cnn.sh
@@ -10,11 +10,14 @@ if [[ -z "${DL_BENCH_ARGS}" ]]; then
 fi
 
 CNNS=(vgg16 resnet18 resnet50 resnext50 resnext101 densenet121 mobilenet_v3l)
-for BS in 0001 0032 0128
+for DTYPE in float32 bfloat16
 do
-    for name in "${CNNS[@]}"
-    do
-        echo "Benchmark $name"
-        benchmark-run -b cnn -p "name='${name}',batch_size='$BS'" --benchmark_desc "${name}_bs$BS" ${DL_BENCH_ARGS} || echo Failed
-    done
+  for BS in 0001 0032 0128
+  do
+      for name in "${CNNS[@]}"
+      do
+          echo "Benchmark $name with BS=$BS and DTYPE=$DTYPE"
+          benchmark-run -b cnn -p "name='${name}',batch_size='$BS'" --dtype "${DTYPE}" --benchmark_desc "${name}_bs$BS" ${DL_BENCH_ARGS} || echo Failed
+      done
+  done
 done
