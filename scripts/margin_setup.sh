0710c6efdf58c049c5e77154629a84eebd8c3c13
Egor <egor.krivov@intel.com>
2024-02-12 18:00:47 +0100

    Added sync for nvidia backend (#84)

---
+++ scripts/margin_setup.sh
@@ -0,0 +1,5 @@
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sh -y ./Miniconda3-latest-Linux-x86_64.sh
+
+# get github repo
+conda install gh -c conda-forge --solver libmamba
