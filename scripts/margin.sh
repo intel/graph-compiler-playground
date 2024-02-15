6ad377ea76700e5bbc409a6dd7810246f43aa1a5
Egor Krivov <egor.krivov@intel.com>
2024-02-15 18:43:30 +0100

    added iteration

--- scripts/margin.sh
+++ scripts/margin.sh
@@ -9,33 +9,35 @@ export OMP_NUM_THREADS=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $
 echo "Cores configured $OMP_NUM_THREADS"
 export ONEDNN_VERBOSE=0
 
-CNNS=(resnet50)
-for COMPILER in dynamo ipex_onednn_graph
+for i in 1 2 3 4 5 6 7
 do
-	for DTYPE in float32 bfloat16
+	CNNS=(resnet50)
+	for COMPILER in dynamo ipex_onednn_graph
 	do
-	  for BS in 0001 0032 0128
-	  do
-	      for name in "${CNNS[@]}"
-	      do
-			echo "Benchmark $name with BS=$BS and DTYPE=$DTYPE"
-			numactl -N 1 benchmark-run -b cnn -p "name='${name}',batch_size='$BS'" --dtype "${DTYPE}" --benchmark_desc "${name}_bs$BS" --host "${HOST}" -c "${COMPILER}" --skip_verification | true
-	      done
-	  done
-	done
-done
-
-
-LLMS=(gptj)
-for COMPILER in dynamo ipex
-do
-	for DTYPE in float32 bfloat16
-	do
-		for name in "${LLMS[@]}"
+		for DTYPE in float32 bfloat16
 		do
-			echo "Benchmark $name with DTYPE=$DTYPE"
-			numactl -N 1 benchmark-run -b llm -p "name='${name}'" --dtype "${DTYPE}" --benchmark_desc "${name}_bs$BS" --host "${HOST}" -c "${COMPILER}" --skip_verification
+		for BS in 0001 0032 0128
+		do
+			for name in "${CNNS[@]}"
+			do
+				echo "Benchmark $name with BS=$BS and DTYPE=$DTYPE"
+				numactl -N 1 benchmark-run -b cnn -p "name='${name}',batch_size='$BS'" --dtype "${DTYPE}" --benchmark_desc "${name}_bs$BS" --host "${HOST}" -c "${COMPILER}" --skip_verification | true
+			done
+		done
+		done
+	done
+
+
+	LLMS=(gptj)
+	for COMPILER in dynamo ipex
+	do
+		for DTYPE in float32 bfloat16
+		do
+			for name in "${LLMS[@]}"
+			do
+				echo "Benchmark $name with DTYPE=$DTYPE"
+				numactl -N 1 benchmark-run -b llm -p "name='${name}'" --dtype "${DTYPE}" --benchmark_desc "${name}_bs$BS" --host "${HOST}" -c "${COMPILER}" --skip_verification
+			done
 		done
 	done
 done
-
