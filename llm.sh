a4515c3e36123bad3ec1ae9a4e3081acc5560973
Egor Krivov <egor.krivov@intel.com>
2024-02-19 14:35:39 +0000

    script

--- llm.sh
+++ llm.sh
@@ -9,7 +9,10 @@ if [[ -z "${DL_BENCH_ARGS}" ]]; then
   exit 1
 fi
 
-for DTYPE in float32 bfloat16
+for NAME in gptj llama2-13b
 do
-  benchmark-run -b llm -p "" --benchmark_desc "gptj" --dtype "${DTYPE}" ${DL_BENCH_ARGS} || echo Failed
+  for DTYPE in float32 bfloat16
+  do
+    benchmark-run -b llm -p "name='${NAME}'" --benchmark_desc "${NAME}" --dtype "${DTYPE}" ${DL_BENCH_ARGS} || echo Failed
+  done
 done
